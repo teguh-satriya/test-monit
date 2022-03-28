3f4aa546db057e2a5d88b0f0321bcb32743181d7
Teguh Satriya <teguh.satriya.w@gmail.com>
2022-03-28 15:23:28 +0700

    fixing Tree

--- TestMonit/Problem5.cs
+++ TestMonit/Problem5.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace TestMonit
 {
-    class Problem5
+    public class Problem5DataModel 
     {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string parent { get; set; }
+        public List<string> children { get; set; }
+    }
+
+    public class Problem5TreeModel
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public Problem5TreeModel parent { get; set; }
+        public List<Problem5TreeModel> children { get; set; }
+    }
+
+    public class Problem5
+    {
+        public static List<Problem5TreeModel> FillRecursive(List<Problem5DataModel> Datas, string ParentID = null)
+        {
+            return Datas.Where(x => x.parent.Equals(ParentID)).Select(item => new Problem5TreeModel
+            {
+                title = item.title,
+                id = item.id,
+                children = FillRecursive(Datas, item.id)
+            }).ToList();
+        }
     }
 }
